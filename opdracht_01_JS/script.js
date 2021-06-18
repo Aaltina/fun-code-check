6bb33a38229a5fec5f02a44d537191989d0daa03
Aaltina <84413944+Aaltina@users.noreply.github.com>
2021-06-18 14:54:43 +0200

    Add files via upload

---
+++ opdracht_01_JS/script.js
@@ -0,0 +1,8 @@
+console.log("Hello Winc Academy");
+let name = "Jet";
+console.log(4 + 4);
+console.log(39 - 25);
+console.log(100 / 4);
+console.log(34 * 8);
+console.log(10 % 300);
+let age = "tweeenvijftig";
