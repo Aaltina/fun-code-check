fa2ee317e2b633b1a83f8e2e24825165dd42ea2f
Aaltina <84413944+Aaltina@users.noreply.github.com>
2021-06-22 22:07:33 +0200

    Add files via upload

---
+++ opdracht_02_js/script.js
@@ -0,0 +1,23 @@
+const age = 19
+
+if (age < 18) {
+    console.log("Je bent te jong en mag niet naar binnen");
+} else if (age > 17) {
+    console.log("Je bent 18 of ouder, je mag naar binnen");
+}
+
+const isFemale = false
+
+if (isFemale) {
+    console.log("Je bent welkom op de ladiesnight");
+} else {
+    console.log("Deze night is for ladies only");
+}
+
+const driverStatus = "bob"
+
+if (driverStatus == "bob") {
+    console.log("Stap maar in de auto, je mag rijden");
+} else {
+    console.log("Lever je sleutels in, je mag niet achter het stuur")
+}
