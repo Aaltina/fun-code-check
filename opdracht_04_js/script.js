fd934d5a8404cb52270b3ec27abebd2bc353fd5e
Aaltina <84413944+Aaltina@users.noreply.github.com>
2021-06-28 13:09:23 +0200

    Add files via upload

--- opdracht_04_js/script.js
+++ opdracht_04_js/script.js
@@ -1,72 +1,24 @@
-const person = {
-    naam: "Jet", age: 51
-}
-const evaluations = [7, 10, 9];
-const colorList = ["red", "green", "gold"];
-colorList.push("black");
-colorList.push(10);
-colorList.push({ greeting: "Hi, I am an object" });
-
-/*console.log (person);
-console.log (person.naam);
-console.log (person.age);
-
-console.log (evaluations);
-
-console.log (colorList);
-console.log (colorList.length);
-console.log (colorList [0]);*/
-console.log(colorList[colorList.length - 1])
-
-const catBreeds = [{
-    name: "Abyssinian",
-    description: "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
-    dog_friendly: 4,
-    energy_level: 5,
-    life_span: "14 - 15",
-    origin: "Egypt",
-    temperament: ["Active", "Energetic", "Independent", "Intelligent", "Gentle"],
-    wikipedia_url: "https://en.wikipedia.org/wiki/Abyssinian_(cat)",
-    food: {
-        favourite_food: "fish",
-        medium_liked_food: "dried fruits",
-        disliked_food: "walnuts"
+const makePizza = function (){
+    console.log ("1. Mix bloem, water en een snufje zout");
+    console.log ("2. Laat het deeg rusten");
+    console.log ("3. Snij de ingredienten en rasp de kaas");
+    console.log ("4. Rol het deeg uit en bestrijk het met tomatensaus");
+    console.log ("5. Beleg de pizza en bak hem");
+    console.log ("6. Geniet van jouw special pizza");
     }
-},
-{
-    name: "Aegean",
-    description: "Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.",
-    dog_friendly: 4,
-    energy_level: 53,
-    life_span: "9- 12",
-    origin: "Greece",
-    temperament: ["Affectionate", "Social", "Intelligent", "Playful", "Active"],
-    wikipedia_url: "https://en.wikipedia.org/wiki/Aegean_cat",
-    food: {
-        favourite_food: "tuna",
-        medium_liked_food: "canned food",
-        disliked_food: "all fruits"
-    }
-},
-{
-    name: "American Bobtail",
-    description: "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.",
-    dog_friendly: 5,
-    energy_level: 3,
-    life_span: "11 - 15",
-    origin: "United States",
-    temperament: ["Intelligent", "Interactive", "Lively", "Playful", "Sensitive"],
-    wikipedia_url: "https://en.wikipedia.org/wiki/American_Bobtail",
-    food: {
-        favourite_food: "meaty things",
-        medium_liked_food: "tuna",
-        disliked_food: "canned food"
-    }
-}
-]
 
-console.log(catBreeds[2].name);
-console.log(catBreeds[0].energy_level);
-console.log(catBreeds[1].temperament[0]);
-console.log(catBreeds[2].temperament[4]);
-console.log(catBreeds[2].food.favourite_food);
+   const makeSushi = function (){
+      console.log ("a. Maak de shushi rijst klaar");
+      console.log ("b. snij de ingerdienten");
+      console.log ("c. Doe de rijst en de ingredienten op de shusi mat");
+      console.log ("d. Rol de shushi");
+      console.log ("e. Proef de Shshi, is het niet te eten volg dan stap f");
+      console.log ("f. Open Thuisbezorgd.nl en bestel Shushi");
+  }
+
+  makeSushi ();
+  makeSushi ();
+  makeSushi ();
+
+  makePizza ();
+  makePizza ();
