f083f84d229b8a02cc506ea6b4713caa11a3ec1b
Aaltina <84413944+Aaltina@users.noreply.github.com>
2021-06-28 15:59:13 +0200

    Add files via upload

---
+++ opdracht_04_js/opdracht_05_js/script.js
@@ -0,0 +1,14 @@
+const paintWalls = function ( collor ){
+console.log ("The wall is painted " + collor);
+}
+
+const pickedCollor ="purple"
+
+paintWalls (pickedCollor);
+
+const paintWallsSide = function (side, collor ){
+    console.log ("The " + side + " wall is painted " + collor);
+    }
+    const wallSide = "west"
+       
+      paintWallsSide (wallSide, pickedCollor);
